44b848d904092764a0475a97ee87793d34ccf712
Ville Halonen <71012693+hzville@users.noreply.github.com>
2021-03-30 00:23:21 +0300

    Get back modified files -> Revert "Revert "week 2 deadline return", push orginal unicafe"
    This reverts commit eedd199a5a23e5f3475d0d1e399a83c657eff746.

---
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -0,0 +1,66 @@
+import unittest
+from maksukortti import Maksukortti
+
+class TestMaksukortti(unittest.TestCase):
+    def setUp(self):
+        self.kortti = Maksukortti(10)
+
+    def test_hello_world(self):
+        self.assertEqual("Hello world", "Hello world")
+
+    def test_konstruktori_asettaa_saldon_oikein(self):
+
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 10 euroa")
+
+
+    def test_syo_edullisesti_vahentaa_saldoa_oikein(self):
+
+        self.kortti.syo_edullisesti()
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 7.5 euroa")
+
+    def test_syo_maukkaasti_vahentaa_saldoa_oikein(self):
+
+        self.kortti.syo_maukkaasti()
+        self.assertEqual(str(self.kortti),"Kortilla on rahaa 6 euroa")
+
+    def test_syo_edullisesti_ei_vie_saldoa_negatiiviseksi(self):
+        self.kortti.syo_edullisesti()
+        self.kortti.syo_edullisesti()
+        self.kortti.syo_edullisesti()
+        self.kortti.syo_edullisesti()
+        self.kortti.syo_edullisesti()
+        self.kortti.syo_edullisesti()
+
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 0.0 euroa")
+
+
+    def test_syo_maukkaasti_ei_vie_saldoa_negatiiviseksi(self):
+
+        self.kortti.syo_maukkaasti()
+        self.kortti.syo_maukkaasti()
+        self.kortti.syo_maukkaasti()
+
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 2 euroa")
+
+    def test_lataa_rahaa(self):
+        self.kortti.lataa_rahaa(10)
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 20 euroa")
+
+    def test_negatiivinen_lataus(self):
+        self.kortti.lataa_rahaa(-10)
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 10 euroa")
+
+    def test_meneeko_yli_150(self):
+        self.kortti.lataa_rahaa(10)
+        self.kortti.lataa_rahaa(222)
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 150 euroa")
+
+    def test_edullinen_tasan_2_5(self):
+        self.kortti = Maksukortti(2.5)
+        self.kortti.syo_edullisesti()
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 0.0 euroa")
+
+    def test_maukas_tasan_4(self):
+        self.kortti = Maksukortti(4)
+        self.kortti.syo_maukkaasti()
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 0 euroa")
